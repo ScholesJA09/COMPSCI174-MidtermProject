a68031d910ab755cf2f88e82180abc7453ef9bc8
jakeyboy214 <boettcheja14@uww.edu>
2026-03-06 02:52:07 -0600

    Stretch Goals 4+5

--- MidtermProject.cpp
+++ MidtermProject.cpp
@@ -7,127 +7,142 @@ int main()
 	//1
 	std::string temp;
 	int convert_num;
-	std::cout << "Type the measurement you want to convert (1 for distance, 2 for temperature, 3 for mass)." << std::endl;
-	std::getline(std::cin, temp);
-	
-	try
-	{
-		convert_num = std::stoi(temp);
-	}
-	catch (std::exception e)
-	{
-		std::cout << "Invalid input, please enter a number." << std::endl;
-		return 1;
-	}
+	while (true) {
+		while (true) {
+			std::cout << "Type the measurement you want to convert (1 for distance, 2 for temperature, 3 for mass)." << std::endl;
+			std::cout << "Type 'exit' or 'quit' to leave" << std::endl;
+			std::getline(std::cin, temp);
+			if (temp == "quit" || temp == "exit") { 
+				std::cout << "Goodbye!" << std::endl;
+				return 0; }
+			else {}
 
-	if (convert_num < 1 || convert_num > 3)
-	{
-		std::cout << "Invalid input, number must be 1, 2 or 3." << std::endl;
-		return 1;
-	}
+			try
+			{
+				convert_num = std::stoi(temp);
+			}
+			catch (std::exception e)
+			{
+				std::cout << "Invalid input, please enter a number." << std::endl;
+				continue;
+			}
 
-	//2
-	int measurement_num;
-	std::cout << "Would you like your value in Metric or Imperial (1 for metric, 2 for imperial)?" << std::endl;
-	std::getline(std::cin, temp);
-
-	try
-	{
-		measurement_num = std::stoi(temp);
-	}
-	catch (std::exception e)
-	{
-		std::cout << "Invalid input, please enter a number." << std::endl;
-		return 2;
-	}
-
-	if (measurement_num < 1 || measurement_num > 2)
-	{
-		std::cout << "Invalid input, number must be 1 or 2." << std::endl;
-		return 2;
-	}
-
-	//3
-	double value;
-	std::cout << "What value do you wish to convert?" << std::endl;
-	std::getline(std::cin, temp);
-
-	try
-	{
-		value = std::stod(temp);
-	}
-	catch (std::exception e)
-	{
-		std::cout << "Invalid input, please enter a number." << std::endl;
-		return 3;
-	}
-
-	//4
-	if (convert_num == 1)
-	{
-		if (value < 0)
-		{
-			std::cout << "Invalid input, distance cannot be negative." << std::endl;
-			return 4;
+			if (convert_num < 1 || convert_num > 3)
+			{
+				std::cout << "Invalid input, number must be 1, 2 or 3." << std::endl;
+				continue;
+			}
+			else { break; }
 		}
-	}
-	else if (convert_num == 2)
-	{
-		if (value < -273.15)
-		{
-			std::cout << "Invalid input, temperature cannot be below absolute zero." << std::endl;
-			return 4;
-		}
-	}
-	else
-	{
-		if (value < 0)
-		{
-			std::cout << "Invalid input, mass cannot be negative." << std::endl;
-			return 4;
-		}
-	}
-	//Validate input to prevent overflow and underflow.
+		//2
+		int measurement_num;
+		while (true) {
+			std::cout << "Would you like your value in Metric or Imperial (1 for metric, 2 for imperial)?" << std::endl;
+			std::getline(std::cin, temp);
 
-	//5
-	double convert_value;
-	if (convert_num == 1)
-	{
-		if (measurement_num == 1)
+			try
+			{
+				measurement_num = std::stoi(temp);
+			}
+			catch (std::exception e)
+			{
+				std::cout << "Invalid input, please enter a number." << std::endl;
+				continue;
+			}
+
+			if (measurement_num < 1 || measurement_num > 2)
+			{
+				std::cout << "Invalid input, number must be 1 or 2." << std::endl;
+				continue;
+			}
+			else { break; }
+		}
+		//3
+		double value;
+		while (true) {
+			std::cout << "What value do you wish to convert?" << std::endl;
+			std::getline(std::cin, temp);
+
+			try
+			{
+				value = std::stod(temp);
+			}
+			catch (std::exception e)
+			{
+				std::cout << "Invalid input, please enter a number." << std::endl;
+				continue;
+			}
+			//4
+			if (convert_num == 1)
+			{
+				if (value < 0)
+				{
+					std::cout << "Invalid input, distance cannot be negative." << std::endl;
+					continue;
+				}
+				else { break; }
+			}
+			else if (convert_num == 2)
+			{
+				if (value < -273.15)
+				{
+					std::cout << "Invalid input, temperature cannot be below absolute zero." << std::endl;
+					continue;
+				}
+				else { break; }
+			}
+			else
+			{
+				if (value < 0)
+				{
+					std::cout << "Invalid input, mass cannot be negative." << std::endl;
+					continue;
+				}
+				else { break; }
+			}
+		}
+		//Validate input to prevent overflow and underflow.
+
+		//5
+		double convert_value;
+		if (convert_num == 1)
 		{
-			convert_value = value * 0.3048;
-			std::cout << "Conversion: " << value << "ft = " << convert_value << "m." << std::endl;
+			if (measurement_num == 1)
+			{
+				convert_value = value * 0.3048;
+				std::cout << "Conversion: " << value << "ft = " << convert_value << "m." << std::endl;
+			}
+			else
+			{
+				convert_value = value / 0.3048;
+				std::cout << "Converstion: " << value << "m. = " << convert_value << "ft." << std::endl;
+			}
+		}
+		else if (convert_num == 2)
+		{
+			if (measurement_num == 1)
+			{
+				convert_value = (value - 32) * 5 / 9;
+				std::cout << "Conversion: " << value << "F = " << convert_value << "C" << std::endl;
+			}
+			else
+			{
+				convert_value = value * 9 / 5 + 32;
+				std::cout << "Conversion: " << value << "C = " << convert_value << "F" << std::endl;
+			}
 		}
 		else
 		{
-			convert_value = value / 0.3048;
-			std::cout << "Converstion: " << value << "m. = " << convert_value << "ft." << std::endl;
-		}
-	}
-	else if (convert_num == 2)
-	{
-		if (measurement_num == 1)
-		{
-			convert_value = (value - 32) * 5 / 9;
-			std::cout << "Conversion: " << value << "F = " << convert_value << "C" << std::endl;
-		}
-		else
-		{
-			convert_value = value * 9 / 5 + 32;
-			std::cout << "Conversion: " << value << "C = " << convert_value << "F" << std::endl;
-		}
-	}
-	else
-	{
-		if (measurement_num == 1)
-		{
-			convert_value = value * 453.592;
-			std::cout << "Conversion: " << value << "lb. = " << convert_value << "g." << std::endl;
-		}
-		else
-		{
-			convert_value = value / 453.592;
-			std::cout << "Conversion: " << value << "g. = " << convert_value << "lb." << std::endl;
+			if (measurement_num == 1)
+			{
+				convert_value = value * 453.592;
+				std::cout << "Conversion: " << value << "lb. = " << convert_value << "g." << std::endl;
+			}
+			else
+			{
+				convert_value = value / 453.592;
+				std::cout << "Conversion: " << value << "g. = " << convert_value << "lb." << std::endl;
+			}
 		}
 	}
 	
